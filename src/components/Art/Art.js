8be1d2cbff8df477a7ee0e572bf194705963a863
Noud63 <noudvandun@gmail.com>
2022-03-16 16:03:20 +0100

    art

--- src/components/Art/Art.js
+++ src/components/Art/Art.js
@@ -3,6 +3,15 @@ import style from './Art.module.css'
 import Menu from '../Menu.js';
 import Namemenu from '../Namemenu.js';
 import Newmenu from '../Newmenu.js'
+import dia1 from '../../images/dia1.jpg'
+import dia1color from '../../images/dia1color.jpg'
+import dia2 from '../../images/dia2.jpg'
+import dia2color from '../../images/dia2color.jpg'
+import dia3 from '../../images/dia3.jpg'
+import dia3color from '../../images/dia3color.jpg'
+import dia4 from '../../images/dia4.jpg'
+import dia4color from '../../images/dia4color.jpg'
+import arrow2 from '../../images/arrow2.png'
 
 const Art = () => {
     return (
@@ -13,6 +22,30 @@ const Art = () => {
                 <Menu />
                 <Newmenu />
             </div>
+            <div className={style.slides}>
+                <div className={style.slide1}>
+                    <img src={dia1} alt="noud van dun" className={style.slide}/>
+                    <img src={dia1color} alt="noud van dun" className={style.slideColor}/>
+                </div>
+                <div className={style.slide2}>
+                    <img src={dia2} alt="noud van dun" className={style.slide}/>
+                    <img src={dia2color} alt="noud van dun" className={style.slideColor} />
+                </div>
+                <div className={style.slide3}>
+                    <img src={dia3} alt="noud van dun" className={style.slide}/>
+                    <img src={dia3color} alt="noud van dun" className={style.slideColor} />
+                </div>
+                <div className={style.slide4}>
+                    <img src={dia4} alt="noud van dun" className={style.slide} />
+                    <img src={dia4color} alt="noud van dun" className={style.slideColor} />
+                </div>
+            </div>
+
+            {/* <div className={style.mywebsite}>
+                <div className={style.shebang1}>The whole shebang at:</div>
+                <img src={arrow2} alt="arrow" className={style.arrow}/>
+                <div className={style.shebang2}>www.noudvandun.com</div>
+            </div> */}
         </div>
     )
 }
--- src/components/Art/Arthome.js
+++ src/components/Art/Arthome.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from './Arthome.module.css';
 import { Link } from "react-router-dom";
-import brush from '../../images/pencilbrush.png'
+import tools from '../../images/tools.png'
 
 const Arthome = () => {
 
@@ -9,16 +9,14 @@ const Arthome = () => {
         <div className={style.artContainer}>
             <div className={style.title}>Art</div>
             <div className={style.wrapper}>
-                <img src={brush} alt="palette" className={style.palette}/>
+                {/* <img src={tools} alt="palette" className={style.palette}/> */}
                 <div className={style.art}>
                     I've been a professional independant artist for many years.<br />
                 If you are interested in art or just curious, have a look.......
-           
-                <Link to={'/art'}>
-                    <button className={style.btn}>Preview and more...</button>
-                </Link>
+           <Link to={'/art'}>
+                        <button className={style.btn}>Preview and more...</button>
+                    </Link>
             </div>
-            
             </div>
         </div>
     )
