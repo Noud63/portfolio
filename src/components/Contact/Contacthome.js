f61c8a8a35fe0e613ae2e0cd253766fb3b29b27f
Noud63 <noudvandun@gmail.com>
2022-03-17 15:07:15 +0100

    fontsize

--- src/components/Contact/Contacthome.js
+++ src/components/Contact/Contacthome.js
@@ -7,22 +7,24 @@ import { Link } from "react-router-dom";
 
 const Contact = () => {
     return (
-          <div className={style.contactHome}>
+        <div className={style.contactHome}>
+
             <div className={style.title}>Contact</div>
+
                 <div className={style.contactWrapper}>
-                <div className={style.mapBox}>
-                    <img src={europe} alt="europe" className={style.map}/>
-                    <div className={style.personal}>
-                        <div>
-                            <div className={style.location}>
+                    <div className={style.mapBox}>
+                       <img src={europe} alt="europe" className={style.map}/>
+                        <div className={style.personal}>
+                           <div>
+                             <div className={style.location}>
                                 <div className={style.iconBox}><img src={location} alt="location" className={style.icon} /></div>
                                 <div className={style.city}>Amsterdam, The Netherlands</div>
-                            </div>
+                             </div>
 
-                            <div className={style.myemail}>
+                             <div className={style.myemail}>
                                 <div className={style.iconBox}><img src={email} alt="" className={style.icon} /></div>
                                 <div className={style.mail}>noudvandun@gmail.com</div>
-                            </div>
+                             </div>
                         </div>
                     </div>
                 </div>
@@ -45,11 +47,8 @@ const Contact = () => {
                     <button type="button" className={style.btn}>Contact me</button>
                 </Link>
                 
-                </div>
-
-            
-
-         </div>
+            </div>
+        </div>
     )
 }
 
